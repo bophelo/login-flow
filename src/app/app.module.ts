07ce52cc8d884b5a8a69a24c2a3775fa6cb4ce56
Bophelo <mpilo@vivaldi.net>
2019-03-06 23:25:26 +0200

    Base Login Flow with Angular Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage';
-
+import { AuthenticationService } from './services/authentication.service';
+import { CrudService } from './services/crud.service';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -17,7 +18,9 @@ import { IonicStorageModule } from '@ionic/storage';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    AuthenticationService,
+    CrudService
   ],
   bootstrap: [AppComponent]
 })
